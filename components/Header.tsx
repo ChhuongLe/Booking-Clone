2b7d8f10f34a6b2db64a58d56de440b919a46ed1
77046302+ChhuongLe@users.noreply.github.com <chhuongcle@gmail.com>
2023-12-15 12:42:22 -0800

    add pop up when hitting stay button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,32 @@
 'use client'
 
-import { Bars3Icon } from "@heroicons/react/24/solid";
+import { Bars3Icon, ChatBubbleLeftIcon, HomeIcon, PaperAirplaneIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import Link from "next/link";
 import { Fragment, useState } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
+
+const products = [
+  {
+    name: "Book a stay",
+    description: "Get a better understanding of your traffic",
+    href: "#",
+    icon: HomeIcon,
+  },
+  {
+    name: "Book a flight",
+    description: "Speak directly to your customers",
+    href: "#",
+    icon: PaperAirplaneIcon,
+  },
+  {
+    name: "Contact our Support Team",
+    description: "Your customers' data will be safe and secure",
+    href: "#",
+    icon: ChatBubbleLeftIcon,
+  }
+]
 
 export default function Header () {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -54,8 +75,22 @@ export default function Header () {
             leaveFrom="opacity-100 translate-y-0"
             leaveTo="opacity-0 translate-y-1"
           >
-
-
+             <Popover.Panel
+              className="absolute bg-white -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl shadow-lg ring-1 ring-gray-900/5"
+             >
+              <div className="p-4">
+                {products.map((item) => (
+                  <div key={item.name} className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6">
+                    <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-gray-200">
+                      <item.icon
+                        className="h-6 w-6 text-[#013894] group-hover:text-blue-600"
+                        aria-hidden="true"
+                      />
+                    </div>
+                  </div>
+                ))}
+              </div>
+          </Popover.Panel>
           </Transition>
         </Popover>
       </Popover.Group>
