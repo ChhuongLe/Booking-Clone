8d3387fbb4959b550ae62a93b56d704789c71f87
77046302+ChhuongLe@users.noreply.github.com <chhuongcle@gmail.com>
2023-12-15 13:21:40 -0800

    add pop up menu to stays tab on large screen devices

--- components/Header.tsx
+++ components/Header.tsx
@@ -60,12 +60,13 @@ export default function Header () {
           <Bars3Icon className="h-6 w-6 aria-hidden:true"/>
         </button>
       </div>
-      <Popover.Group className="hidden lg:flex lg:gap-x-2">
+      <Popover.Group className="hidden lg:flex lg:gap-x-12">
         <Popover className="relative">
           <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-white">
             Stays
             <ChevronDownIcon className="h-5 w-5 flex-non text-white" aria-hidden="true" />
           </Popover.Button>
+
           <Transition
             as={Fragment}
             enter="transition ease-out duration-200"
@@ -76,17 +77,27 @@ export default function Header () {
             leaveTo="opacity-0 translate-y-1"
           >
              <Popover.Panel
-              className="absolute bg-white -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl shadow-lg ring-1 ring-gray-900/5"
+              className="absolute bg-white top-full -left-96 z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl shadow-lg ring-1 ring-gray-900/5"
              >
               <div className="p-4">
                 {products.map((item) => (
-                  <div key={item.name} className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6">
+                  <div key={item.name} className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50">
                     <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-gray-200">
                       <item.icon
-                        className="h-6 w-6 text-[#013894] group-hover:text-blue-600"
+                        className="h-6 w-6 text-[#013B94] group-hover:text-blue-600"
                         aria-hidden="true"
                       />
                     </div>
+                      <div className="flex-auto">
+                        <a
+                          href={item.href}
+                          className="block font-semibold text-[#013B94]"
+                        >
+                          {item.name}
+                          <span className="absolute inset-0"/>
+                        </a>
+                        <p className="mt-1 text-[#013B94]">{item.description}</p>
+                      </div>
                   </div>
                 ))}
               </div>
